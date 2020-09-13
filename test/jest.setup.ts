8ae5d43be172db0a4dc7a0d9b4eadc42ea4fc66d
Ashok <ashokpatidar80@gmail.com>
2020-09-13 17:35:33 +0530

    👷 mocking redis 👷

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,14 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import IORedis from 'ioredis-mock'
+
 import { AppModule } from '../src/app/app.module';
+import { RedisService } from 'src/config/redis/redis';
 
 let app: NestExpressApplication;
+
+// create redis instance on redis mock lib
+const redis = new IORedis()
+// override getConfig function of redis services
+const mockRedisServices = {
+  async getConfig() {
+    return new RedisPubSub({
+      publisher: redis.createConnectedClient(),
+      subscriber: redis,
+    })
+  },
+}
 
 beforeAll(async () => {
   console.log("Jest Setup Initialize")
   const moduleFixture: TestingModule = await Test.createTestingModule({
     imports: [AppModule],
-  }).compile();
+  })
+  .overrideProvider(RedisService)
+  .useValue(mockRedisServices)
+  .compile();
   app = moduleFixture.createNestApplication();
   global.app = app
   await app.init();
